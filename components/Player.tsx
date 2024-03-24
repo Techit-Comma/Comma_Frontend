68f3e6da15b7a89955ca21f5ab93b0a5af5a3ba8
ppusda <ppusda1234@gmail.com>
2024-03-24 22:38:54 +0900

    fix: Type 변경 Song => AlbumData

--- components/Player.tsx
+++ components/Player.tsx
@@ -13,26 +13,14 @@ interface PlayerProps {
 export const Player = () => {
     const player = usePlayer();
     const activeAlbum = player.activeAlbum;
-    const songUrl = player.activeAlbum?.fileUrl;
 
-    const convertAlbumDataToSong = (albumData: AlbumData): Song => {
-        return {
-            id: albumData.id.toString(),
-            user_id: "", // user_id 값이 AlbumData에 없으므로, 적절한 값을 설정해야 합니다.
-            author: albumData.artistNickname,
-            title: albumData.albumname,
-            song_path: albumData.fileUrl,
-            image_path: albumData.imgUrl,
-        };
-    };
-
-    if(!songUrl || !activeAlbum){
+    if(!activeAlbum){
         return null
     }
 
     return (
         <div className='fixed bottom-0 left-0 right-0 bg-black h-[80px]'>
-            <PlayerContent song={convertAlbumDataToSong(activeAlbum)} songUrl={songUrl}/>
+            <PlayerContent album={activeAlbum}/>
         </div>
     )
 }
