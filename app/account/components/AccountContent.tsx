8dbc55eb1d390dca8f7c4a75d02da25ceaa5e102
ppusda <ppusda1234@gmail.com>
2024-03-25 15:56:03 +0900

    feat: credit, donation 페이지 연결

--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -5,7 +5,7 @@ import React, {useEffect, useState} from "react";
 import {ListItemButton, ListItemText} from "@mui/material";
 import {CheckAccessToken, LogoutProcess} from "@/libs/auth";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faDoorOpen, faGears, faLock} from "@fortawesome/free-solid-svg-icons";
+import {faDoorOpen, faGears, faLock, faCoins, faHandHoldingDollar} from "@fortawesome/free-solid-svg-icons";
 import {toast} from "react-hot-toast";
 import {useRecoilState} from "recoil";
 import {loginState} from "@/providers/RecoilContextProvider";
@@ -43,6 +43,12 @@ const AccountContent = () => {
                 <h1 className='text-white text-xl font-semibold m-5 mb-2.5'>보안 및 개인정보 보호</h1>
                 <ListItemButton component="a" onClick={() => router.push("/account/modify")} className="mx-3 font-semibold">
                     <FontAwesomeIcon icon={faGears} className="mr-3 w-8" size="lg"/><ListItemText primary="개인정보 설정"/>
+                </ListItemButton>
+                <ListItemButton component="a" onClick={() => router.push("/credit")} className="mx-3 font-semibold">
+                    <FontAwesomeIcon icon={faCoins} className="mr-3 w-8" size="lg"/><ListItemText primary="크레딧 페이지" />
+                </ListItemButton>
+                <ListItemButton component="a" onClick={() => router.push("/account/donation")} className="mx-3 font-semibold">
+                    <FontAwesomeIcon icon={faHandHoldingDollar} className="mr-3 w-8" size="lg"/><ListItemText primary="후원 페이지" />
                 </ListItemButton>
                 <ListItemButton component="a" onClick={() => router.push("/account/modify/password")} className="mx-3 font-semibold">
                     <FontAwesomeIcon icon={faLock} className="mr-3 w-8" size="lg"/><ListItemText primary="비밀번호 변경하기" />
