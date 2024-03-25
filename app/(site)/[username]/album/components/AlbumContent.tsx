4ca09da657485137873793204961a481d036d836
ppusda <ppusda1234@gmail.com>
2024-03-25 15:56:30 +0900

    fix: Like Button 제거

--- app/(site)/[username]/album/components/AlbumContent.tsx
+++ app/(site)/[username]/album/components/AlbumContent.tsx
@@ -33,7 +33,6 @@ const AlbumContent = ({albums}:Props) => {
                     <div className="flex-1">
                         <MediaItem onClick={(id)=>onPlay(album)} data={album}/>
                     </div>
-                    <LikeButton songId={album.id}/>
                 </div>
             ))}
             <div className="absolute bottom-8 right-10">
