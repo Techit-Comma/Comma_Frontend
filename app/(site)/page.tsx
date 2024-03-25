a4d05bfb1c3a1d3224387f042a60d5c5dec8a22c
ppusda <ppusda1234@gmail.com>
2024-03-26 00:30:44 +0900

    fix: 좋아요 표시한 노래 항목 제거

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -85,9 +85,6 @@ export default function Main() {
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">COM,MA</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem image='/images/liked.png' name='좋아요 표시한 노래' href='liked'/>
-          </div>
         </div>
       </Header>
       {isLogin ? (
