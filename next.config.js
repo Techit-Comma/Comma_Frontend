8516788dfbeae983db18c0f8e3c8b6384999bdcc
ppusda <ppusda1234@gmail.com>
2024-03-25 16:21:41 +0900

    fix: github, google 이미지 접근되도록 수정

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 
     images: {
-        domains: ['kv6d2rdb2209.edge.naverncp.com', 'kr.object.ncloudstorage.com'],
+        domains: ['kv6d2rdb2209.edge.naverncp.com', 'kr.object.ncloudstorage.com', 'avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
     },
 };
 
